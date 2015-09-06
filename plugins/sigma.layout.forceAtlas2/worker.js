bbb6c353b129e487c405fb3058aab48b04115458
bogklug <bogklug@tosh.poraje>
2015-09-06 20:41:27 +0200

    - Debugging.

--- plugins/sigma.layout.forceAtlas2/worker.js
+++ plugins/sigma.layout.forceAtlas2/worker.js
@@ -891,6 +891,9 @@
               NodeMatrix[np(n, 'y')] + NodeMatrix[np(n, 'dy')] *
               (nodespeed / W.settings.slowDown);
           }
+	    else {
+		console.log("zafiksowany!", n)
+	    }
         }
       }
       else {
