f2c685ed23a48d876cd52c28f96ba870de35fcca
bogklug <bogklug@gmail.com>
2017-09-11 17:04:19 +0200

    - Linear texture filtering.

--- src/misc/sigma.misc.texturing.js
+++ src/misc/sigma.misc.texturing.js
@@ -69,12 +69,18 @@
     });
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    // gl.texParameteri(gl.TEXTURE_2D,
+    //                  gl.TEXTURE_MAG_FILTER,
+    //                  gl.NEAREST); //gl.LINEAR);
+    // gl.texParameteri(gl.TEXTURE_2D,
+    //                  gl.TEXTURE_MIN_FILTER,
+    //                  gl.NEAREST); //gl.LINEAR); //gl.LINEAR_MIPMAP_NEAREST);
     gl.texParameteri(gl.TEXTURE_2D,
                      gl.TEXTURE_MAG_FILTER,
-                     gl.NEAREST); //gl.LINEAR);
+                     gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D,
                      gl.TEXTURE_MIN_FILTER,
-                     gl.NEAREST); //gl.LINEAR); //gl.LINEAR_MIPMAP_NEAREST);
+                     gl.LINEAR); //gl.LINEAR_MIPMAP_NEAREST);
     //gl.generateMipmap(gl.TEXTURE_2D);
     gl.bindTexture(gl.TEXTURE_2D, null);
     this.needs_reloading = false;
